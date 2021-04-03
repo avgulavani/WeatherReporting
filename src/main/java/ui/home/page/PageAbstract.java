fe15664633384d113448d0785491922acdd61204
Ashish <gulavaniashish2008@gmail.com>
2021-04-03 14:07:25 +0530

    added missing page abstract class file

--- src/main/java/ui/home/page/PageAbstract.java
+++ src/main/java/ui/home/page/PageAbstract.java
@@ -1,15 +1,15 @@
-package weather;
+package ui.home.page;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public abstract class WetherPageComponent {
+public abstract class PageAbstract {
 		
 		protected WebDriverWait wait;
 		
-		public WetherPageComponent(final WebDriver driver) {
-			this.wait=new WebDriverWait(driver,30);
+		public PageAbstract(final WebDriver driver) {
+			this.wait=new WebDriverWait(driver,60);
 			PageFactory.initElements(driver, this);			
 		}
 
