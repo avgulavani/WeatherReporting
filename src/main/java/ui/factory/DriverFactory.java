01258506f33d0b49855e75da9cfc1c064e0e35e4
Ashish <gulavaniashish2008@gmail.com>
2021-04-04 19:10:44 +0530

    change in driverfactory class for remote executeion

--- src/main/java/ui/factory/DriverFactory.java
+++ src/main/java/ui/factory/DriverFactory.java
@@ -1,20 +1,45 @@
 package ui.factory;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
+	
+	private static WebDriver driver;
 
 	private static final Supplier<WebDriver> chromeSupplier = () -> {	
 		
-		//System.getProperty("user.dir")+"\\chrome\\ChromeDriver.exe");
+		
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		System.setProperty("webdriver.chrome.driver","/Users/a.vitthal.gulavani/Desktop/Personal_Data/personal_workspace/chromedriver-2");
-		return new ChromeDriver();
+		String host = "localhost";
+		
+			if (System.getProperty("HUB_HOST") != null) {
+				host = System.getProperty("HUB_HOST");
+			}
+			else
+				return new ChromeDriver();
+			
+			String completeURL = "http://" + host + ":4444/wd/hub";
+			
+			try {
+				driver = new RemoteWebDriver(new URL(completeURL), capabilities);
+			} catch (MalformedURLException e) {
+			    
+				e.printStackTrace();
+			}
+		
+			return driver;
 
 	};
 	
